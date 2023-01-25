1e8692e976d79b742e2045a4c6a5e0db94214a95
Your Name <ladraniful10@gmail.com>
2023-01-26 00:23:30 +0100

    add mongoDB and connect it with mongoose

--- employees/employees.js
+++ employees/employees.js
@@ -1,71 +1,86 @@
 const express = require('express')
 const joi = require('joi')
+const mongoose =require('mongoose')
 
 const router = express.Router()
-
-const employees = [
-  {id:1, fullname:"amine ladrani", salery:3000},
-  {id:2, fullname:"dado ladrani", salery:4000},
-  {id:3, fullname:"omar ladrani", salery:2000}
-]
+//add a schema
+const Employee = mongoose.model('Employee', new mongoose.Schema({
+  fullname:{
+    type:String,
+    required:true,
+    minlength:3,
+    maxlength:44
+  },
+  salery:{
+    type:Number,
+    required:true
+  }
+}))
 
 //get all employees 
-router.get('/',(req,res)=>{
-res.send(employees)
+router.get('/',async(req,res)=>{
+  const employees = await Employee.find()
+  res.send(employees)
 })
 //get employee by id
-router.get('/:id',(req,res)=>{
-const employee = employees.find( emp => emp.id == req.params.id)
+router.get('/:id',async(req,res)=>{
+const employee = await Employee.find({_id:req.params.id})
 if(!employee){
-return res.send('sorry we dont have this user in our database')
+  return res.send('this user is not exist in our database')
 }
 res.send(employee)
 })
 
 //add new employees
-router.post('/', (req,res)=>{
+router.post('/', async(req,res)=>{
 const schema = joi.object( {
-  id:joi.number().integer().min(4).required(),
   fullname: joi.string().min(3).required(),
   salery:joi.number().integer().required()
 })
 
-//  const catcherr =  schema.validate(req.body);
  const {error} = schema.validate(req.body);
 
 if(error){
 return res.send(error.message)
 }
   
-const newemployee = {
-id:req.body.id,
-fullname:req.body.fullname,
-salery:req.body.salery
-}
-employees.push(newemployee)
+const newemployee = new Employee({
+  fullname:req.body.fullname,
+  salery:req.body.salery
+  })
+await newemployee.save()
 res.send(newemployee)
 })
 
 //uptade an employee by id
-router.put('/:id',(req,res)=>{
-const employee = employees.find(emp => emp.id == req.params.id)
-if(!employee){
-return res.send("couldn't found employee")
-}
-employee.fullname = req.body.fullname
-employee.salery = req.body.salery
+router.put('/:id',async(req,res)=>{
+        const schema = joi.object({
+          fullname:joi.string().required(),
+          salery:joi.number().integer().required()
+        })
 
-res.send(employee)
+        const {error} = schema.validate(req.body)
+        if(error){
+          return res.send(error.message)
+        }
+    
+        //
+      const employee = await Employee.findByIdAndUpdate(req.params.id,{
+        fullname:req.body.fullname
+      },{new:true})
+
+      if(!employee){
+        return res.send('invalide id')
+      }
+      res.send(employee)
 })
 
 //remove an employee
-router.delete('/:id',(req,res)=>{
-const employee = employees.find(emp => emp.id == req.params.id)
+router.delete('/:id',async(req,res)=>{
+const employee = await Employee.findByIdAndRemove(req.params.id)
 if(!employee){
-return res.send("we couldn't found this employee")
+  return res.status(404).send("invalid id")
 }
-const idxOfRmemployee = employees.indexOf(employee)
-employees.splice(idxOfRmemployee,1)
 res.send(employee)
 })
 
--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const log1 = require('./logger/logging')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const employees = require('./employees/employees')
+const mongoose = require('mongoose')
 const app = express()
 //for devlopment envirement 
 if(app.get('env') === 'development'){
     app.use(morgan('tiny'))
     app.use(log1)
 }
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://localhost:27017/expDB", { useNewUrlParser: true })
+.then(()=> console.log("connected to database"))
+.catch((error)=>console.error("there is an error" + error))
 
 //using the middlewares
 app.use(helmet())
 app.use(express.json())
-
 app.use('/', employees)
 
 //running the server
