5211182982523aa03a2e722890f16c3056b8cc13
Your Name <ladraniful10@gmail.com>
2023-01-25 02:26:08 +0100

    rest full api with array of objects

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+      const employees = [
+        {id:1, fullname:"amine ladrani", salery:3000},
+        {id:2, fullname:"dado ladrani", salery:4000},
+        {id:3, fullname:"omar ladrani", salery:2000}
+      ]
+
+      //get all employees 
 app.get('/',(req,res)=>{
-  res.send('welcome to home page')
+  res.send(employees)
+})
+    //get employee by id
+app.get('/:id',(req,res)=>{
+  const employee = employees.find( emp => emp.id == req.params.id)
+  if(!employee){
+    return res.send('sorry we dont have this user in our database')
+  }
+  res.send(employee)
 })
 
-app.listen(3000 , ()=>{
-  console.log('app ronning on port 3000...')
+  //add new employees
+  app.post('/', (req,res)=>{
+  console.log(req.body)
+  const newemployee = {
+    id:req.body.id,
+    fullname:req.body.fullname,
+    salery:req.body.salery
+  }
+  employees.push(newemployee)
+  res.send(newemployee)
+})
+
+//uptade an employee by id
+app.put('/:id',(req,res)=>{
+  const employee = employees.find(emp => emp.id == req.params.id)
+  if(!employee){
+    return res.send("couldn't found employee")
+  }
+  employee.fullname = req.body.fullname
+  employee.salery = req.body.salery
+
+  res.send(employee)
+})
+
+//remove an employee
+
+//running the server
+const port = process.env.port || 3000
+app.listen(port , ()=>{
+    console.log(`app ronning on port ${port}...`)
 })
