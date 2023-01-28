30469062fd2175798fb79a6c8506f321d9f6bcdb
Your Name <ladraniful10@gmail.com>
2023-01-28 22:51:24 +0100

    hash the password

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,36 @@
 const express = require('express')
 const router = express.Router()
 const {User , validateUser} = require('../model/user')
+const _ = require('lodash')
+const bcrypt = require('bcrypt')
 
 //add new user
 router.post('/' , async(req,res)=>{
  try{
-  const err =  validateUser(req.body)
-  if(err){
-   return res.send(err.message)
-  }
-   //add the new user
-   const user = new User({
-       fullname:req.body.fullname,
-       email:req.body.email,
-       password:req.body.password
-   })
-   await user.save()
-   res.send(user)
+      const err =  validateUser(req.body)
+      if(err){
+          return res.send(err.message)
+      }
+      //we check if we have this email inn our database
+      let user = await User.findOne({email:req.body.email})
+      //in the case i have this user in my database
+      if(user){
+          return res.send('we have this user in our database')
+      }
+
+      //add the new user
+        //i use pick for just accepting the fullname and email and password requests
+        user = new User(_.pick(req.body , ['fullname','email','password']))
+        
+        //hash the password using bcrypt
+        const saltRound = 10
+        const salt = await bcrypt.genSalt(saltRound)
+        user.password = await bcrypt.hash(user.password , salt)
+
+        //save the user in our database
+      await user.save()
+        //i used pick to response with just fullname and email that come from user
+      res.send(_.pick(user , ['fullname','email']))
  }catch(err){
   return res.send('there is an err you should fix it')
  }
